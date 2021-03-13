087f43f7d8eb59ccc029f7dd58d160eaddd31902
jedgodsey <jed.godsey@gmail.com>
2021-03-12 20:33:20 -0800

    created hero component

---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+const Hero = () => {
+  <div id="hero">
+    <div id="hero-logo"><img src="" /></div>
+    <div id="welcome-box">
+      <p id="welcome-text">WELCOME TO</p>
+      <p id="hero-text">Rooster Grin</p>
+      <div id="hero-button">BUTTON BUTTON</div>
+    </div>
+  </div>
+}
+
+export default Hero
