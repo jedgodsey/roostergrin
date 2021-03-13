6b5b80af4005fb7ed9ebb4a0069c0038d782cd97
jedgodsey <jed.godsey@gmail.com>
2021-03-13 11:17:18 -0800

    grid completed

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -2,6 +2,11 @@ import orangebutton from '../assets/orange-button.png';
 import help from '../assets/help.jpg';
 import laptop from '../assets/laptop.jpg';
 import girl from '../assets/girl.jpg';
+import purplelorem from '../assets/purple-lorem.png';
+import goggles from '../assets/goggles.png';
+import mask from '../assets/facemask.png';
+import calendar from '../assets/calendar.png';
+import clipboard from '../assets/clipboard.png';
 
 const Grid = () => {
   return (
@@ -10,63 +15,64 @@ const Grid = () => {
         <div className="lorem">
           <h3>LOREM IPSUM</h3>
           <p>Lorem ipsum dolor sit amet, et felis integer. Cras ac, duis nisl magna est sociis, neque in odio vel, sit lobortis erat. Fugit quam, ut pede ut ante, in viverra eros dictum nisl ligula, lacus est vehicula donec. Quam in ac quam. Duis et, non arcu imperdiet sem tellus suspendisse. Condimentum non aliquet sed, quisque risus vitae semper duis feugiat .</p>
-          <h6>&gt; Lorem Ipsum</h6>
+          <img src={purplelorem} className="purple-lorem" />
         </div>
       </div>
       <div className="grid-image bounding">
         <img src={help} className="full-image" />
       </div>
-      <div className="grid-wide" id="laptop">
+      <div className="grid-wide bounding" id="laptop">
         <img src={laptop} className="full-image" />
-        <div className="button-box">
-          <h3>LOREM IPSUM</h3>
-          <img src={orangebutton} className="button" />
+        <div className="content">
+          <div className="button-box">
+            <h3>LOREM IPSUM</h3>
+            <img src={orangebutton} className="button" />
+          </div>
+          <div className="offset-box"></div>
         </div>
-        <div className="offset-box"></div>
       </div>
       <div className="lorem-cell">
-      <div className="lorem">
+        <div className="lorem">
           <h3>LOREM IPSUM DOLOR SIT AMET</h3>
           <p>Lorem ipsum dolor sit amet, et felis integer. Cras ac, duis nisl magna est sociis, neque in odio vel, sit lobortis erat. Fugit quam, ut pede ut ante, in viverra eros dictum nisl ligula.</p>
-          <h6>&gt; Lorem Ipsum</h6>
+          <img src={purplelorem} className="purple-lorem" />
         </div>
       </div>
       <div id="icons">
         <div id="icons-box">
-          <div className="subject-box">
-            <div className="icon-box">
-              <img src="" />
+          <div className="icon-row">
+            <div className="subject-box">
+              <img src={clipboard} className="icons" />
+              <h5>LOREM</h5>
             </div>
-            <h5>LOREM</h5>
+            <div className="subject-box">
+              <img src={mask} className="icons" />
+              <h5>LOREM IPSUM</h5>
+            </div>
           </div>
-          <div className="subject-box">
-            <div className="icon-box">
-              <img src="" />
+          <div className="icon-row">
+            <div className="subject-box">
+              <img src={goggles} className="icons" />
+              <h5>LOREM IPSUM</h5>
             </div>
-            <h5>LOREM IPSUM</h5>
-          </div>
-          <div className="subject-box">
-            <div className="icon-box">
-              <img src="" />
+            <div className="subject-box">
+              <img src={calendar} className="icons" />
+              <h5>LOREM</h5>
             </div>
-            <h5>LOREM IPSUM</h5>
-          </div>
-          <div className="subject-box">
-            <div className="icon-box">
-              <img src="" />
-            </div>
-            <h5>LOREM</h5>
           </div>
         </div>
       </div>
       <div className="grid-wide bounding" id="girl">
         <img src={girl} className="full-image" />
-        <div className="offset-box"></div></div>
-        <div className="button-box">
-          <h3>LOREM IPSUM</h3>
-          <p>Lorem ipsum dolor sit amet, et felis integer. Cras ac, duis nisl magna est sociis, neque in odio vel, sit lobortis erat. Fugit quam, ut pede ut ante, in viverra eros dictum nisl ligula.</p>
-          <img src={orangebutton} className="button" />
+        <div className="content">
+          <div className="offset-box"></div>
+          <div className="button-box">
+            <h3>LOREM IPSUM</h3>
+            <p>Lorem ipsum dolor sit amet, et felis integer. Cras ac, duis nisl magna est sociis, neque in odio vel, sit lobortis erat. Fugit quam, ut pede ut ante, in viverra eros dictum nisl ligula.</p>
+            <img src={orangebutton} className="button" />
+          </div>
         </div>
+      </div>
     </div>
   )
 }
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,15 +6,17 @@ const Hero = () => {
   return (
     <div id="hero" className="bounding">
       <img src={hero} className="full-image" />
-      <div id="top-box">
-        <div id="logo-box"><img src={whitelogo} id="white-logo" /></div>
-      </div>
-      <div id="welcome-box">
-        <h1 id="welcome-text">WELCOME TO</h1>
-        <h2 id="hero-text">Rooster Grin</h2>
-        <a href="">
-          <img src={purplebutton} className="button" />
-        </a>
+      <div className="content">
+        <div id="top-box">
+          <div id="logo-box"><img src={whitelogo} id="white-logo" /></div>
+        </div>
+        <div id="welcome-box">
+          <h1 id="welcome-text">WELCOME TO</h1>
+          <h2 id="hero-text">Rooster Grin</h2>
+          <a href="">
+            <img src={purplebutton} className="button" />
+          </a>
+        </div>
       </div>
     </div>
   )
