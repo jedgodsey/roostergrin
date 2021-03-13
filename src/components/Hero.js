1c97adf885c451e101a0a672f6ea2fae1a4eda25
jedgodsey <jed.godsey@gmail.com>
2021-03-13 15:02:19 -0800

    hero animating

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,14 +5,14 @@ import purplebutton from '../assets/purple-button.png';
 const Hero = () => {
   return (
     <div id="hero" className="bounding">
-      <img src={hero} className="full-image" />
+      <img src={hero} className="full-image" id="hero-fade" />
       <div className="hero-content">
         <div id="top-box">
           <div id="logo-box"><img src={whitelogo} id="white-logo" /></div>
         </div>
         <div id="welcome-box">
-          <h1 id="welcome-text">WELCOME TO</h1>
-          <h2 id="hero-text">Rooster Grin</h2>
+          <h1 id="welcome-text" className="turn-white">WELCOME TO</h1>
+          <h2 id="hero-text" className="turn-white">Rooster Grin</h2>
           <a href="">
             <img src={purplebutton} className="button" />
           </a>
